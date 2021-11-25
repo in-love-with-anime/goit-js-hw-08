6e8dc2a2cd5893b131dc5768e05523068bebec35
in-love-with-anime <vall.bell91@gmail.com>
2021-11-26 01:43:08 +0200

    add hw

---
+++ src/js/02-video.js
@@ -0,0 +1,21 @@
+const iFrameEl = document.querySelector('#vimeo-player');
+const throttle = require('lodash.throttle');
+
+// Инициализация плеера
+const player = new Vimeo.Player(iFrameEl);
+
+// Сохранение времени воспроизведения в локальное хранилище
+const onTimeUpdate = date => {
+    localStorage.setItem('videoplayer-current-time', date.seconds);
+    console.log(parseInt(localStorage.getItem('videoplayer-current-time')));
+};
+
+// Обновление времени воспроизведения
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+// Возобновление воспроизведения с сохраненной позиции
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then().catch();
+
+
+// 2 вариант
+// window.onload = player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
---
+++ src/js/03-feedback.js
@@ -0,0 +1,40 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const formInputMail = document.querySelector('.feedback-form input');
+const formInputText = document.querySelector('.feedback-form textarea')
+
+
+const FORM_KEY = 'feedback-form-state';
+const formData = {};
+
+
+const onFormSubmit = evt => {
+    evt.preventDefault();
+    evt.target.reset();
+    console.log(FORM_KEY, JSON.parse(localStorage.getItem(FORM_KEY)));
+    localStorage.removeItem(FORM_KEY);
+}
+
+const onFormInput = evt => {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+}
+
+const savingValue = () => {
+        const savedMessage = JSON.parse(localStorage.getItem(FORM_KEY));
+    
+        if (savedMessage && savedMessage.message) {
+            formInputText.value = savedMessage.message;
+        };
+        
+        if (savedMessage && savedMessage.email) {
+            formInputMail.value = savedMessage.email;
+        };
+    };
+
+savingValue();
+    
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
